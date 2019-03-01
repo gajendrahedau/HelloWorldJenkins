b3fc0aec63dccd41f5379fa441868c8c83f1f3ce
nikhil <nikhil.barigala@gmail.com>
2019-03-01 16:04:32 +0530

    Fourth Commit

--- HelloWorldJenkins/src/com/cg/hello/clients/HelloWorldExample.java
+++ HelloWorldJenkins/src/com/cg/hello/clients/HelloWorldExample.java
@@ -1,6 +1,6 @@
 package com.cg.hello.clients;
 public class HelloWorldExample {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Jenkins.");
+		System.out.println("Welcome");
 	}
 }
